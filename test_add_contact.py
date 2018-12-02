66c40c063f7b21a286916618add6469ac304e77d
Sergey Ushnurtsev <oceanite@yandex.ru>
2018-12-02 20:40:59 +0300

    test_add_contact: Extracted methods

--- test_add_contact.py
+++ test_add_contact.py
@@ -8,9 +8,10 @@ class TestAddContact(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_contact(self):
-        wd = self.wd
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
@@ -19,7 +20,11 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_add_contact_page(self, wd):
         wd.find_element_by_link_text("add new").click()
+
+    def create_contact(self, wd):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("3")
@@ -37,7 +42,17 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("email").send_keys("123@test.ru")
         wd.find_element_by_xpath("//body").click()
         wd.find_element_by_xpath("(//input[@value='Enter'])").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_add_contact_page(wd)
+        self.create_contact(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
