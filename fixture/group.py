a95a152e4e76c8b23cc4fc58d820438ac5b354bd
Sergey Ushnurtsev <oceanite@yandex.ru>
2018-12-10 20:48:01 +0300

    Created conftest and new test test_del_group.py

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,7 @@ class GroupHelper():
 
     def create(self, group):
         wd = self.app.wd
-        self.app.open_groups_page()
+        self.open_groups_page()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -24,7 +24,13 @@ class GroupHelper():
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
-        self.return_to_groups_page()
