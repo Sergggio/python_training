61b282eaa71c09ca98c8f6878a85bab0f9a5865d
Sergey Ushnurtsev <oceanite@yandex.ru>
2018-12-10 19:52:56 +0300

    Created class for Groups (group.py)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="test1", header="header1", footer="comment1"))
+    app.group.create(Group(name="test1", header="header1", footer="comment1"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
