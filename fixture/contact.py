7204bb7b25d68206170edeb30340153ee0bc4373
Sergggio <sergggio@github.com>
2018-12-12 13:42:47 +0300

    Added new test case: edit first contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,4 @@
-
+import time
 
 class ContactHelper():
 
@@ -31,3 +31,18 @@ class ContactHelper():
         wd = self.app.wd
         wd.find_element_by_xpath("(//input[@value='Delete'])").click()
         wd.switch_to_alert().accept()
+
+    def edit(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("(//*[@title='Edit'])").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("update").click()
