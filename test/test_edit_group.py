4eeddc93ff0f9264ddf0bec75e64d44c40d633f8
Sergggio <oceanite@yandex.ru>
2018-12-18 10:49:31 +0300

    Renamed to modify_group, added If and refactoring

--- test/test_edit_group.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from model.group import Group
-
-
-def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit(Group(name="test_edited", header="header_edited", footer="comment_edited"))
-    app.session.logout()
