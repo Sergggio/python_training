44f5c3e1ee8e7b46cc97e451a51af966e13a9a3e
Ushnurtsev Sergey <ushnurtsev@bpcbt.com>
2018-12-26 12:08:34 +0300

    Added asserts to the group tests

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,12 +3,22 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="temporary_group", header="header", footer="comment"))
-    app.group.modify_first_group(Group(name="test_edited"))
+    group = Group(name="New_group_edited")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="temporary_group", header="header", footer="comment"))
-    app.group.modify_first_group(Group(header="header_edited"))
+# def test_modify_group_header(app):
+#     old_groups = app.group.get_group_list()
+#     if app.group.count() == 0:
+#         app.group.create(Group(header="header_edited"))
+#     app.group.modify_first_group(Group(header="header_edited"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
