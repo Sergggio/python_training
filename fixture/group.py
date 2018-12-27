53146b1105841763e95d946a38421c605b05efd6
Ushnurtsev Sergey <ushnurtsev@bpcbt.com>
2018-12-27 18:01:10 +0300

    Added cache and hash for groups

--- fixture/group.py
+++ fixture/group.py
@@ -12,6 +12,7 @@ class GroupHelper():
         wd.find_element_by_name("new").click()
         self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -37,6 +38,7 @@ class GroupHelper():
         self.select_first_group(wd)
         wd.find_element_by_name("delete").click()
         self.open_groups_page()
+        self.group_cache = None
 
     def select_first_group(self, wd):
         wd.find_element_by_name("selected[]").click()
@@ -53,19 +55,24 @@ class GroupHelper():
         self.fill_group_form(new_group_data)
         wd.find_element_by_name("update").click()
         self.open_groups_page()
+        self.group_cache = None
 
     def count(self):
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        #$$('span.group')
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            #$$('span.group')
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
+
