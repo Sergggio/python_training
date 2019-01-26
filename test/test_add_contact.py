9aa51e96cd6be1c9ac237b946c15c9ac36b57e6c
Sergey Ushnurtsev <oceanite@yandex.ru>
2019-01-26 19:20:01 +0300

    Task 15. Added parameterization for adding contact and group

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Name1", lastname="LastName1",
-                      home_phone="1234567", mobile_phone="1234", work_phone="12345", secondary_phone="123456")
-    app.contact.create(contact)
-    new_contacts = app.contact.get_contact_list()
-    print(new_contacts)
-    assert len(old_contacts) + 1 == app.contact.count()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="", lastname="", home_phone="",
+                    mobile_phone="", work_phone="", secondary_phone="", email="")] + \
+           [Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), home_phone=random_string("homephone", 10),
+            mobile_phone=random_string("mobile_phone", 10), work_phone=random_string("work_phone", 10),
+            secondary_phone=random_string("secondary_phone", 10), email=random_string("email", 20))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
+     old_contacts = app.contact.get_contact_list()
+     app.contact.create(contact)
+     new_contacts = app.contact.get_contact_list()
+     print(new_contacts)
+     assert len(old_contacts) + 1 == app.contact.count()
+     old_contacts.append(contact)
+     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
