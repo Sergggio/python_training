9ea76cbd38515e46169030cab9c984a10a987161
Sergey Ushnurtsev <oceanite@yandex.ru>
2019-02-02 16:46:58 +0300

    Task 20. Modifyed test_del_group using DB

--- fixture/group.py
+++ fixture/group.py
@@ -43,6 +43,14 @@ class GroupHelper():
         self.open_groups_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        wd.find_element_by_name("delete").click()
+        self.open_groups_page()
+        self.group_cache = None
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
@@ -50,6 +58,11 @@ class GroupHelper():
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
 
     #def return_to_groups_page(self):
     #     wd = self.app.wd
